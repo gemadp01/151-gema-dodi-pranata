58b5b2a0e78b02d974534f0ee184ec95819dbc9b
Gema Dodi Pranata <gemadp01@gmail.com>
2024-05-30 13:38:23 +0700

    update: register n login views, card n button components

--- resources/views/components/product/card.blade.php
+++ resources/views/components/product/card.blade.php
@@ -1,24 +1,33 @@
 @props([
+'href',
 'name',
 'price',
-'product_owner',
-'product_image',
-'profil_image'
+'image',
+'stock'
 ])
 
-<div class="bg-white rounded-lg shadow-lg overflow-hidden max-w-72 w-full">
-    <img src="{{ $product_image }}" alt="{{ $name }}" class="w-full h-48 object-cover">
-    <div class="p-4">
-        <h2 class="text-xl text-gray-800 mb-2 ">{{ $name }}</h2>
-        <p class="text-2xl text-gray-700 font-bold leading-tight mb-4">
-            Rp. {{ $price }}
-        </p>
-        <div class="flex justify-between items-center">
-            <div class="flex items-center">
-                <img src="{{ $profil_image }}" alt="{{ $product_owner }}" class="w-8 h-8 rounded-full mr-2">
-                <span class="text-sm text-gray-800 ">{{ $product_owner }}</span>
-            </div>
-            {{-- <span class="text-gray-600">2 hours ago</span> --}}
+<div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <a href="{{ $href }}">
+        <img src="{{ $image }}" alt="{{ $name }}" class="h-52 w-72 object-cover rounded-t-xl" />
+        <div class="px-4 py-3 w-72">
+            <span class="text-gray-400 mr-3 text-xs">
+                {{ $stock }}
+                @if ($stock <= 50) items remaining @else left @endif </span>
+                    <p class="text-lg font-bold text-black truncate block capitalize">{{ $name }}</p>
+                    <div class="flex items-center">
+                        <p class="text-lg font-semibold text-black cursor-auto my-3">Rp. {{ $price }}</p>
+                        {{-- <del>
+                            <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                        </del> --}}
+                        <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                                <path
+                                    d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                            </svg></div>
+                    </div>
+            </span>
         </div>
-    </div>
+    </a>
 </div>
